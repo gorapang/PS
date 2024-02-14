cc67a40b3dd4be6e4570d5416196d09ea86f2f63
gorapang <107031994+gorapang@users.noreply.github.com>
2024-02-14 22:52:41 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 3036 ms, Memory: 330172 KB -BaekjoonHub

--- 백준/Gold II/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold II/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -7,32 +7,55 @@ using namespace std;
 
 int n;
 long long ans = 0;
-int a[4000], b[4000], c[4000], d[4000];
 
-long long count()
+int count_element(vector<long long> v, int num)
 {
-	long long cnt = 0;
+	int tmp = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i] == num)
+			tmp++;
+	}
+	return tmp;
+}
+
+long long count(vector<long long> A, vector<long long> B, vector<long long> C, vector<long long> D)
+{
 	vector<long long> v, w;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			v.push_back(a[i] + b[j]);
-			w.push_back(c[i] + d[j]);
+			v.push_back(A[i] + B[j]);
+			w.push_back(C[i] + D[j]);
 		}
 	}
 
-	sort(w.begin(), w.end());
+	std::sort(v.begin(), v.end());
+	std::sort(w.begin(), w.end());
 
-	for (int i = 0; i < v.size(); i++)
-	{
-		long long target = -v[i];
-		int low = lower_bound(w.begin(), w.end(), target) - w.begin();
-		int high = upper_bound(w.begin(), w.end(), target) - w.begin();
-		cnt += (high - low);
+	long long start = 0, end = v.size() - 1; //2 pointer
+	long long result = 0, sum;
+	while (start < v.size() && end >= 0) {
+		sum = v[start] + w[end];
+		if (sum > 0) end--;
+		else if (sum == 0) {
+			long long ab = 1, cd = 1;
+			while (start < v.size() - 1 && v[start] == v[start + 1]) {
+				start++;
+				ab++;
+			}
+			while (end > 0 && w[end] == w[end - 1]) {
+				end--;
+				cd++;
+			}
+			result += ab * cd;
+			start++;
+		}
+		else start++;
 	}
 
-	return cnt;
+	return result;
 }
 
 int main()
@@ -40,14 +63,15 @@ int main()
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> n;
+	vector<long long> A(n);
+	vector<long long> B(n);
+	vector<long long> C(n);
+	vector<long long> D(n);
 	for (int i = 0; i < n; i++)
 	{
-		cin >> a[i] >> b[i] >> c[i] >> d[i];
+		cin >> A[i] >> B[i] >> C[i] >> D[i];
 	}
-
-	
-
-	ans += count();
+	ans += count(A, B, C, D);
 
 	cout << ans << '\n';
 	return 0;
