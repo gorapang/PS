cbf82880728dc1dd14a2127a3d1b5b9a73600480
gorapang <107031994+gorapang@users.noreply.github.com>
2024-04-30 13:12:05 +0900

    [Silver II] Title: 과일 탕후루, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int cnt[10]; //cnt[i]: 과일i의 개수
+
+bool okay(int cnt[10]) //과일이 3개 이상이면 false
+{
+	int res = 0;
+	for (int i = 1; i <= 9; i++)
+	{
+		if (cnt[i] > 0) res++;
+	}
+
+	if (res <= 2) return true;
+	else return false;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N;
+	cin >> N;
+	
+	int ans = 0, start = 0, end = 0;
+
+	vector<int> v(N);
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	while (end < N)
+	{
+		if (okay(cnt))
+			cnt[v[end++]]++;
+
+		else cnt[v[start++]]--;
+
+		if (okay(cnt)) ans = max(ans, end - start);
+		
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
