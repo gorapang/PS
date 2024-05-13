9d4b866d158beff4b754d41a6b4be127854a1193
gorapang <107031994+gorapang@users.noreply.github.com>
2024-05-13 16:46:12 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 8 ms, Memory: 13,372 KB -BaekjoonHub

---
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int tc;
+		cin >> tc;
+		
+		int score[101] = { 0, };
+		for (int i = 0; i < 1000; i++)
+		{
+			int n;
+			cin >> n;
+			score[n]++;
+		}
+
+		int ans = -1;
+		int max_score = 0;
+
+		for (int i = 0; i <= 100; i++)
+		{
+			if (score[i] >= max_score)
+			{
+				max_score = score[i];
+				ans = i;
+			}
+		}
+		
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
+
+	return 0;
+}
