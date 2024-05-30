72158c965f0dbbfd3b36eb97db8fba48f35faaea
Jeongju Park <107031994+gorapang@users.noreply.github.com>
2024-05-30 16:35:03 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int arr[9];
+
+
+void backtracking(int num, int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+
+		return;
+	}
+
+	int last = 0;
+
+
+	for (int i = num; i < n; i++)
+	{
+		if (v[i] != last)
+		{
+			arr[k] = v[i];
+			last = arr[k];
+			backtracking(i, k + 1);
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	v.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+	backtracking(0, 0);
+
+	return 0;
+}
