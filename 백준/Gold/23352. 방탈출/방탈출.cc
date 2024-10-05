505a8db931c81b9c762187e3fabd1bbfc68f5044
Jeongju Park <107031994+gorapang@users.noreply.github.com>
2024-10-05 19:36:33 +0900

    [Gold V] Title: 방탈출, Time: 112 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/23352. 방탈출/방탈출.cc
+++ 백준/Gold/23352. 방탈출/방탈출.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 int arr[50][50];
-int visited[50][50];  // 방문 여부와 depth를 함께 관리
+bool visited[50][50]; 
 int n, m;
 int max_dist;
 
@@ -17,7 +17,7 @@ void init()
 {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            visited[i][j] = 0;
+            visited[i][j] = false;
 }
 
 void bfs(int x, int y)
@@ -26,9 +26,8 @@ void bfs(int x, int y)
     queue<pair<int, int>> q;
     q.push({ x, y });
 
-    init();
-    visited[x][y] = 1;  // 시작점을 방문처리하면서 depth 1로 설정
-
+    init();  
+    visited[x][y] = true;  
     int depth = 0;
 
     while (!q.empty())
@@ -40,16 +39,14 @@ void bfs(int x, int y)
             int y = q.front().second;
             q.pop();
 
-            // 최장 거리(depth)를 갱신할 때마다 sum 계산
             if (depth > max_dist) {
                 max_dist = depth;
-                sum = arr[sx][sy] + arr[x][y];  // 현재까지의 최대 거리에서의 합
+                sum = arr[sx][sy] + arr[x][y]; 
             }
             else if (depth == max_dist) {
                 sum = max(sum, arr[sx][sy] + arr[x][y]);
             }
 
-            // 4방향 탐색
             for (int dir = 0; dir < 4; dir++)
             {
                 int nx = x + dx[dir];
@@ -58,14 +55,13 @@ void bfs(int x, int y)
                 if (nx >= n || nx < 0 || ny >= m || ny < 0 || visited[nx][ny] || arr[nx][ny] == 0)
                     continue;
 
-                visited[nx][ny] = 1;  // 새로운 위치를 방문 처리
+                visited[nx][ny] = true;  
                 q.push({ nx, ny });
             }
         }
-        depth++;  // 모든 현재 큐에 있는 노드를 처리한 후 깊이를 1 증가
+        depth++;
     }
 }
-
 
 int main()
 {
