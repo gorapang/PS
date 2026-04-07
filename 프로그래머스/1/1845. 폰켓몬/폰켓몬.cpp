45f9c844c1c73248a4d29365597f55e4a26ba36b
Jeongju Park <107031994+gorapang@users.noreply.github.com>
2026-04-07 21:04:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    unordered_set <int> st;
+    
+    
+    int n = nums.size();
+    
+    return answer;
+}
